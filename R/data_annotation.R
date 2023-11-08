6537faaf596e5b6db76225c41f7ffd1c3dcfbd0a
Ashenafee <ashenafeem@gmail.com>
2023-11-08 12:57:49 -0500

    Refactor variable names in annotateIAD function

--- R/data_annotation.R
+++ R/data_annotation.R
@@ -6,19 +6,19 @@
 #'
 #' @param data A dataframe of position of the fish and the stimulus over time.
 #'             The dataframe should have the following columns:
-#'             - x_fish
-#'             - y_fish
-#'             - x_stim
-#'             - y_stim
+#'             - xFish
+#'             - yFish
+#'             - xStim
+#'             - yStim
 #'             - time
 #' @return An annotated dataframe.
 #' @export
 annotateIAD <- function(data) {
     # Calculate distance from stimulus
-    data$distance <- sqrt((data$x_fish - data$x_stim)^2 +
-                              (data$y_fish - data$y_stim)^2)
+    data$distance <- sqrt((data$xFish - data$xStim)^2 +
+                              (data$yFish - data$yStim)^2)
     # Calculate angle from stimulus
-    data$angle <- atan2(data$y_fish - data$y_stim, data$x_fish - data$x_stim)
+    data$angle <- atan2(data$yFish - data$yStim, data$xFish - data$xStim)
     # Calculate time since stimulus
     data$time_since_stim <- data$time - data$time[1]
     return(data)
