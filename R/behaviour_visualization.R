de7fa15408a371086fec8f9efc02eb701b9bde08
Ashenafee <ashenafeem@gmail.com>
2023-11-08 12:58:13 -0500

    Add animation function for fish and stimulus movement

--- R/behaviour_visualization.R
+++ R/behaviour_visualization.R
@@ -5,19 +5,19 @@
 #' It plots the x and y coordinates of the fish and the stimulus, showing how
 #' the fish responds to the stimulus.
 #'
-#' @param behaviourData A dataframe with columns: x_fish, y_fish, x_stim,
-#' y_stim, time.
+#' @param behaviourData A dataframe with columns: xFish, yFish, xStim,
+#' yStim, time.
 #' @return A plot of the trajectory of fish and stimulus movement.
 #' @export
 #' @examples
 #' plotTrajectory(behaviourData)
 plotTrajectory <- function(behaviourData) {
-    plot(behaviourData$x_fish, behaviourData$y_fish,
+    plot(behaviourData$xFish, behaviourData$yFish,
         type = "l", col = "blue",
         xlab = "X Coordinate", ylab = "Y Coordinate",
         main = "Fish Movement Trajectory"
     )
-    lines(behaviourData$x_stim, behaviourData$y_stim, type = "l", col = "red")
+    lines(behaviourData$xStim, behaviourData$yStim, type = "l", col = "red")
     legend("topright",
         legend = c("Fish", "Stimulus"),
         col = c("blue", "red"), lty = 1, cex = 0.8
@@ -29,15 +29,15 @@ plotTrajectory <- function(behaviourData) {
 #' Calculates and plots the distance between the fish and the stimulus over
 #' time, providing insight into the fish's response to the stimulus.
 #'
-#' @param behaviourData A dataframe with columns: x_fish, y_fish, x_stim,
-#' y_stim, time.
+#' @param behaviourData A dataframe with columns: xFish, yFish, xStim,
+#' yStim, time.
 #' @return A plot showing the distance of the fish from the stimulus over time.
 #' @export
 #' @examples
 #' plotDistanceOverTime(behaviourData)
 plotDistanceOverTime <- function(behaviourData) {
-    distances <- sqrt((behaviourData$x_fish - behaviourData$x_stim)^2 +
-                          (behaviourData$y_fish - behaviourData$y_stim)^2)
+    distances <- sqrt((behaviourData$xFish - behaviourData$xStim)^2 +
+                          (behaviourData$yFish - behaviourData$yStim)^2)
     plot(behaviourData$time, distances,
         type = "b", pch = 19, col = "green",
         xlab = "Time", ylab = "Distance from Stimulus",
@@ -51,7 +51,7 @@ plotDistanceOverTime <- function(behaviourData) {
 #' indicating areas of high and low activity.
 #' It requires the `ggplot2` library to create a 2D binning heatmap.
 #'
-#' @param behaviourData A dataframe with columns: x_fish, y_fish.
+#' @param behaviourData A dataframe with columns: xFish, yFish.
 #' @return A heatmap of fish activity.
 #' @export
 #' @examples
@@ -59,8 +59,8 @@ plotDistanceOverTime <- function(behaviourData) {
 heatmapActivity <- function(behaviourData) {
     library(ggplot2)
     ggplot(behaviourData, aes(
-        x = x_fish,
-        y = y_fish
+        x = xFish,
+        y = yFish
     )) +
         geom_bin2d(bins = 30) +
         scale_fill_gradient(low = "blue", high = "red") +
@@ -69,4 +69,45 @@ heatmapActivity <- function(behaviourData) {
             title = "Heatmap of Fish Activity"
         ) +
         theme_minimal()
+}
+
+#' Animation of Fish and Stimulus Movement
+#'
+#' This function creates an animation of the movement paths of both the fish
+#' and the stimulus over time. It plots the x and y coordinates of the fish and
+#' the stimulus, showing how the fish responds to the stimulus.
+#'
+#' @param behaviourData A dataframe with columns: xFish, yFish, xStim,
+#' yStim, time.
+#' @param filename The name of the file where the animation will be saved.
+#' @return Saves the animation to a file.
+#' @export
+#' @examples
+#' animateMovement(behaviourData, "animation.gif")
+animateMovement <- function(behaviourData, filename = "animation.gif") {
+    library(ggplot2)
+    library(gganimate)
+    library(readr)
+    library(gifski)
+    library(png)
+
+    # Explicitly set the column variables
+    xFish <- behaviourData$xFish
+    yFish <- behaviourData$yFish
+    xStim <- behaviourData$xStim
+    yStim <- behaviourData$yStim
+
+    p <- ggplot() +
+        geom_point(data = behaviourData, aes(x = xFish, y = yFish,
+                                             color = "Fish")) +
+        geom_point(data = behaviourData, aes(x = xStim, y = yStim,
+                                             color = "Stimulus")) +
+        scale_color_manual(values = c("Fish" = "blue", "Stimulus" = "red")) +
+        labs(color = "Type")
+    p <- p + transition_time(time) + labs(title = "Time: {frame_time}")
+
+    g <- animate(p, duration = 5, fps = 20, width = 200, height = 200,
+                 renderer = gifski_renderer())
+
+    anim_save(filename, g)
 }
