93012fb297d51f7aaefe83207cde4242b3fdd932
Ashenafee <ashenafeem@gmail.com>
2023-11-12 01:10:06 -0500

    Add option to fetch pathway as XML and add function to display pathway in Cytoscape

--- R/access_wikipathways.R
+++ R/access_wikipathways.R
@@ -18,13 +18,34 @@ selectPathwayById <- function(pathwayId) {
 #' This function fetches a specific pathway from WikiPathways.
 #'
 #' @param pathwayId A character string representing the ID of the pathway.
+#' @param asXml A boolean indicating whether the pathway should be returned as
+#' XML.
 #' @return The GPML file of the pathway.
 #' @export
 #' @examples
 #' fetchPathwayById("WP554")
-fetchPathwayById <- function(pathwayId) {
+fetchPathwayById <- function(pathwayId, asXml = TRUE) {
+    # Use the WikiPathways API to fetch the pathway
     pathwayString <- rWikiPathways::getPathway(pathwayId)[[1]]
-    parsedPathway <- XML::xmlParse(pathwayString)
 
-    return(parsedPathway)
+    # Check if the pathway should be returned as XML
+    if (asXml) {
+        pathwayString <- XML::xmlParse(pathwayString)
+    }
+    return(pathwayString)
+}
+
+#' Display Pathway by ID
+#'
+#' This function displays a specific pathway from WikiPathways through the
+#' Cytoscape application (https://cytoscape.org/).
+#'
+#' @param pathwayId A character string representing the ID of the pathway.
+#' @return The GPML file of the pathway.
+#' @export
+#' @examples
+#' displayPathwayById("WP554")
+displayPathwayById <- function(pathwayId) {
+    RCy3::commandsRun(paste("wikipathways import-as-pathway id=",
+                            pathwayId, sep = ""))
 }
