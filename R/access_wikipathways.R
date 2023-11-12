b5daa3e87d5d91a0faf4acaf68ce3600d5f098d6
Ashenafee <ashenafeem@gmail.com>
2023-11-12 00:58:06 -0500

    Add function to fetch pathway by ID from WikiPathways

--- R/access_wikipathways.R
+++ R/access_wikipathways.R
@@ -12,3 +12,19 @@ selectPathwayById <- function(pathwayId) {
     pathway <- pathways[pathways$id == pathwayId, ]
     return(pathway)
 }
+
+#' Fetch Pathway by ID
+#'
+#' This function fetches a specific pathway from WikiPathways.
+#'
+#' @param pathwayId A character string representing the ID of the pathway.
+#' @return The GPML file of the pathway.
+#' @export
+#' @examples
+#' fetchPathwayById("WP554")
+fetchPathwayById <- function(pathwayId) {
+    pathwayString <- rWikiPathways::getPathway(pathwayId)[[1]]
+    parsedPathway <- XML::xmlParse(pathwayString)
+
+    return(parsedPathway)
+}
