716a40f598b2f46a52eb3fb82e2d7763b066de9f
Ashenafee <ashenafeem@gmail.com>
2023-12-11 04:35:46 -0500

    Refactor getGeneInfo by creating helpers

--- R/getGeneInfo.R
+++ R/getGeneInfo.R
@@ -11,7 +11,6 @@
 #' getGeneInfo(c("raraa", "rargb"))
 #' @export
 getGeneInfo <- function(geneSymbols) {
-
     # Check if geneSymbols is a character vector
     if (!is.character(geneSymbols)) {
         stop("geneSymbols must be a character vector")
@@ -58,6 +57,12 @@ getGeneInfo <- function(geneSymbols) {
     # Create a list to store the Gene objects
     geneObjects <- list()
 
+    # Create a named list of AnatomyList objects
+    anatomyLists <- lapply(geneSymbols, function(x) {
+        new("AnatomyList", anatomy = list())
+    })
+    names(anatomyLists) <- geneSymbols
+
     # Create a named list of OntologyList objects
     ontologyLists <- lapply(geneSymbols, function(x) {
         new("OntologyList", ontologies = list())
@@ -74,31 +79,30 @@ getGeneInfo <- function(geneSymbols) {
             next
         }
 
-        # Loop through the rows of gene information for the current gene symbol
-        for (i in seq_len(nrow(geneInfo))) {
-            # Create a new Ontology object if there's a valid GO ID
-            if (geneInfo$go_id[i] == "") {
-                next
-            }
-            ontology <- new("Ontology",
-                id = geneInfo$go_id[i],
-                name = geneInfo$name_1006[i],
-                definition = geneInfo$definition_1006[i],
-                namespace = geneInfo$namespace_1003[i],
-                description = geneInfo$description[i]
-            )
+        # Get the ZFIN ID for the current gene symbol
+        zfinID <- geneInfo$zfin_id_id[1]
 
-            # Add the Ontology object to the OntologyList object for the current
-            # gene symbol
-            ontologyLists[[geneSymbol]] <-
-                AtlaZ::addOntology(ontologyLists[[geneSymbol]], ontology)
-        }
+        # Add anatomy information to the AnatomyList object for the current
+        # gene symbol
+        anatomyInfo <- getZfinBackgroundInfo(zfinID)$anatomy
+        anatomyLists[[geneSymbol]] <- createAnatomyObjects(
+            anatomyInfo,
+            anatomyLists[[geneSymbol]]
+        )
+
+        # Add ontology information to the OntologyList object for the current
+        # gene symbol
+        ontologyLists[[geneSymbol]] <- createOntologyObjects(
+            geneInfo,
+            ontologyLists[[geneSymbol]]
+        )
 
         # Create a new Gene object
         gene <- new("Gene",
             geneSymbol = geneSymbol,
             ensemblID = geneInfo$ensembl_gene_id[1],
             zfinID = geneInfo$zfin_id_id[1],
+            anatomy = anatomyLists[[geneSymbol]],
             ontology = ontologyLists[[geneSymbol]]
         )
 
@@ -107,6 +111,66 @@ getGeneInfo <- function(geneSymbols) {
     }
 
     return(geneObjects)
+}
+
+
+#' Add anatomy information to a list of AnatomyList objects
+#'
+#' @param anatomyInfo A data frame of anatomy information.
+#' @param anatomyLists A list of AnatomyList objects.
+#' @return A list of AnatomyList objects.
+createAnatomyObjects <- function(anatomyInfo, anatomyLists) {
+    for (i in seq_len(nrow(anatomyInfo))) {
+        # Skip the current row if there is no anatomy ID
+        if (anatomyInfo$id[i] == "") {
+            next
+        }
+
+        # Skip the current row if the type is not "Term"
+        if (anatomyInfo$type[i] != "Term") {
+            next
+        }
+
+        # Create a new Anatomy object
+        anatomy <- new(
+            "Anatomy",
+            id = anatomyInfo$id[i],
+            label = anatomyInfo$label[i],
+            description = anatomyInfo$description[i]
+        )
+
+        # Add the Anatomy object to the list of Anatomy objects
+        anatomyLists <- AtlaZ::addAnatomy(anatomyLists, anatomy)
+    }
+
+    return(anatomyLists)
+}
+
+#' Add ontology information to a list of OntologyList objects
+#'
+#' @param geneInfo A data frame of gene information.
+#' @param ontologyLists A list of OntologyList objects.
+#' @return A list of OntologyList objects.
+createOntologyObjects <- function(geneInfo, ontologyLists) {
+    for (i in seq_len(nrow(geneInfo))) {
+        # Create a new Ontology object if there's a valid GO ID
+        if (geneInfo$go_id[i] == "") {
+            next
+        }
+        ontology <- new("Ontology",
+            id = geneInfo$go_id[i],
+            name = geneInfo$name_1006[i],
+            definition = geneInfo$definition_1006[i],
+            namespace = geneInfo$namespace_1003[i],
+            description = geneInfo$description[i]
+        )
+
+        # Add the Ontology object to the OntologyList object for the current
+        # gene symbol
+        ontologyLists <- AtlaZ::addOntology(ontologyLists, ontology)
+    }
+
+    return(ontologyLists)
 }
 
 # [END]
