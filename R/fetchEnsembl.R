59d7bbc30b2b2cd909ea0246a91635b06b9d5f60
Ashenafee <ashenafeem@gmail.com>
2023-12-11 00:50:34 -0500

    Refactor getGeneInfo function to retrieve gene information for multiple gene symbols

--- R/fetchEnsembl.R
+++ R/fetchEnsembl.R
@@ -1,56 +1,69 @@
 #' Access gene information from Ensembl Version 110
 #'
-#' @param geneSymbol A character string representing the gene symbol.
-#' @return A data frame containing gene information.
-#' @examples
-#' getGeneInfo("rara")
-#' getGeneInfo("rargb")
+#' This function retrieves gene information from Ensembl Version 110 for the
+#' provided gene symbols. It uses the biomaRt package to access the zebrafish
+#' gene dataset from Ensembl. The gene information is returned as a list of data
+#' frames, where each data frame contains information on a single gene.
 #'
+#' @param geneSymbols A character vector representing gene symbols.
+#' @return A list of data frames of information on each gene.
+#' @examples
+#' getGeneInfo(c("raraa", "rargb"))
 #' @export
-getGeneInfo <- function(geneSymbol) {
+getGeneInfo <- function(geneSymbols) {
     # Use the zebrafish gene dataset from Ensembl
-    ensembl <- biomaRt::useEnsembl(biomart = "genes",
-                                   dataset = "drerio_gene_ensembl",
-                                   version = "110")
+    ensembl <- biomaRt::useEnsembl(
+        biomart = "genes",
+        dataset = "drerio_gene_ensembl",
+        version = "110"
+    )
 
-    # Fetch the gene information using biomaRt
-    results <- biomaRt::getBM(attributes = c("external_gene_name",
-                                             "external_transcript_name",
-                                             "zfin_id_id",
-                                             "zfin_id_symbol",
-                                             "zfin_id_trans_name",
-                                             "ensembl_gene_id",
-                                             "ensembl_transcript_id",
-                                             "ensembl_peptide_id",
-                                             "description",
-                                             "start_position",
-                                             "end_position",
-                                             "strand",
-                                             "transcript_start",
-                                             "transcript_end"),
-                              filter = "external_gene_name", value = geneSymbol,
-                              mart = ensembl)
+    # Fetch the gene information using biomaRt for all gene symbols
+    results <- biomaRt::getBM(
+        attributes = c(
+            "external_gene_name",
+            "external_transcript_name",
+            "zfin_id_id",
+            "zfin_id_symbol",
+            "zfin_id_trans_name",
+            "ensembl_gene_id",
+            "ensembl_transcript_id",
+            "ensembl_peptide_id",
+            "go_id",
+            "name_1006",
+            "definition_1006",
+            "namespace_1003",
+            "description",
+            "start_position",
+            "end_position",
+            "strand",
+            "transcript_start",
+            "transcript_end"
+        ),
+        filter = "external_gene_name", value = geneSymbols,
+        mart = ensembl
+    )
 
-    # Return the results
-    return(results)
-}
+    # Create a list where each entry is a data frame containing information on a
+    # single gene
 
-#' Verify that a gene symbol is valid
-#'
-#' @param geneSymbol A character string representing the gene symbol.
-#' @param geneInfo A data frame containing gene information.
-#' @return A boolean indicating whether the gene symbol is valid.
-#' @examples
-#' geneInfo <- getGeneInfo("rara")
-#' isGeneSymbol("rara", geneInfo)
-#'
-#' geneInfo <- getGeneInfo("rargb")
-#' isGeneSymbol("rargb", geneInfo)
-#'
-#' @export
-isGeneSymbol <- function(geneSymbol, geneInfo) {
-    # If the query returns any results, the gene symbol is valid
-    return(any(geneInfo$external_gene_name == geneSymbol))
+    # Create a list of gene symbols
+    geneSymbols <- unique(results$external_gene_name)
+
+    # Create a list of data frames
+    geneInfo <- lapply(geneSymbols, function(geneSymbol) {
+        # Filter the results to only include the current gene symbol
+        geneInfo <- results[results$external_gene_name == geneSymbol, ]
+
+        # Return the gene information
+        return(geneInfo)
+    })
+
+    # Make the genes accessible by their symbol
+    names(geneInfo) <- geneSymbols
+
+    # Return the gene information
+    return(geneInfo)
 }
 
 # [END]
